JKatzwinkel/archiv-editor

commit 9c8ff8bb12e0a3e9cf5f41ff5076a889c7b0ec1a

new local object repo ingest rewritten
new objects ingest now sorts objects according to linking
structure, then ingests type-wise.
podl serialization: concurrence elements
should fix 3497, 3720

org.bbaw.pdr.ae.control.core/src/org/bbaw/pdr/ae/control/core/XMLProcessor.java
@@ -114,7 +114,7 @@ public class XMLProcessor implements XMLProcessorInterface
 	 * @param person 
 	 * @throws XMLStreamException the xML stream exception
 	 */
-	private void createNode(final XMLEventWriter eventWriter, final String name, final Concurrence c, PdrMetaObject person)
+	private void createPersonConcurrenceNode(final XMLEventWriter eventWriter, final String name, final Concurrence c, PdrMetaObject person)
 			throws XMLStreamException
 	{
 		XMLEventFactory eventFactory = XMLEventFactory.newInstance();
@@ -131,7 +131,7 @@ public class XMLProcessor implements XMLProcessorInterface
 		{
 			for (int j = 0; j < c.getReferences().size(); j++)
 			{
-				createNode(eventWriter, "validationStm", c.getReferences().get(j), "podl",
+				createValidationStmNode(eventWriter, "validationStm", c.getReferences().get(j), "podl",
 						"http://pdr.bbaw.de/namespaces/podl/", person);
 			}
 		}
@@ -1348,23 +1348,26 @@ public class XMLProcessor implements XMLProcessorInterface
 	 * @param validationStm the validation statement
 	 * @param prefix the prefix
 	 * @param uri the uri
-	 * @param aspect 
+	 * @param pdrObject 
 	 * @throws XMLStreamException the xML stream exception
 	 */
-	private void createNode(final XMLEventWriter eventWriter, final String name, final ValidationStm validationStm,
-			final String prefix, final String uri, PdrMetaObject aspect) throws XMLStreamException
+	private void createValidationStmNode(final XMLEventWriter eventWriter, final String name, final ValidationStm validationStm,
+			final String prefix, final String uri, PdrMetaObject pdrObject) throws XMLStreamException
 	{
 		// create validationStm element
 		XMLEventFactory eventFactory = XMLEventFactory.newInstance();
 		// Create Start node
 		StartElement sElement = eventFactory.createStartElement(prefix, uri, name);
-
+		
 		eventWriter.add(sElement);
+		String auth = null;
 		if (validationStm.getAuthority() == null) {
 			buflog("Aodl warning: validationStm without authority. falling back to revision authority");  
-			eventWriter.add(eventFactory.createAttribute("authority", aspect.getRecord().getRevisions().get(0).getAuthority().toString()));
-		} else
-			eventWriter.add(eventFactory.createAttribute("authority", validationStm.getAuthority().toString()));
+			// XXX: first or last revision?
+			auth = pdrObject.getRecord().getRevisions().get(0).getAuthority().toString();
+		} else 
+			auth = validationStm.getAuthority().toString();
+		eventWriter.add(eventFactory.createAttribute("authority", auth));
 
 
 		if (validationStm.getReference() != null) {
@@ -1380,8 +1383,11 @@ public class XMLProcessor implements XMLProcessorInterface
 				eventWriter.add(eventFactory.createAttribute("quality", ref.getQuality()));
 
 			// XXX authority attribute is not even a part in aodl schema reference definition...!
-			//if (ref.getAuthority() != null)
-				//eventWriter.add(eventFactory.createAttribute("authority", ref.getAuthority().toString()));
+			// XXX but it is in podl schema!!!
+			if (ref.getAuthority() != null) {
+				eventWriter.add(eventFactory.createAttribute("authority", ref.getAuthority().toString()));
+			} else
+				eventWriter.add(eventFactory.createAttribute("authority", auth));
 
 			// Create Content
 			if (ref.getSourceId() != null) {
@@ -1827,7 +1833,7 @@ public class XMLProcessor implements XMLProcessorInterface
 		{
 			for (int i = 0; i < a.getValidation().getValidationStms().size(); i++)
 			{
-				createNode(eventWriter, "validationStm", a.getValidation().getValidationStms().get(i), "aodl",
+				createValidationStmNode(eventWriter, "validationStm", a.getValidation().getValidationStms().get(i), "aodl",
 						"http://pdr.bbaw.de/namespaces/aodl/", a);
 			}
 		}
@@ -1970,7 +1976,7 @@ public class XMLProcessor implements XMLProcessorInterface
 
 				{
 
-					createNode(eventWriter, "concurrence", p.getConcurrences().getConcurrences().get(i), p);
+					createPersonConcurrenceNode(eventWriter, "concurrence", p.getConcurrences().getConcurrences().get(i), p);
 				}
 			}
 			eventWriter.add(eventFactory

org.bbaw.pdr.ae.metamodel/src/org/bbaw/pdr/ae/metamodel/PdrId.java
@@ -230,7 +230,8 @@ public class PdrId implements Comparable<PdrId>, Cloneable
 	}
 
 	/**
-	 * getter for _type.
+	 * Returns String identifier of object type, usually one of <code>{'pdrRo','pdrPo','pdrAo',...}</code>,
+	 * which is the first of the <code>'.'</code>-separated fields of a full {@link PdrId} string representation. 
 	 * @return _type.
 	 */
 	public final String getType()

commit 5bb08f69a945b2cb45d4c3ad1d90d2574dc00388

fix issue 4273: invalid aspect XML, impossible update of modified aspects
commit 9c8ff8bb12e0a3e9cf5f41ff5076a889c7b0ec1a broke aspect XML serialization
in an attempt to fix issues 3497 and 3720. XML of any aspect edited within
AE was caused to be invalid by that. Hence, no aspect edited within AE could
possibly be uploaded/ingested to remote.
This hopefully makes everything right again.

org.bbaw.pdr.ae.control.core/src/org/bbaw/pdr/ae/control/core/XMLProcessor.java
@@ -1373,21 +1373,25 @@ public class XMLProcessor implements XMLProcessorInterface
 		if (validationStm.getReference() != null) {
 			// XXX aodl schema actually does not limit references to only one per validationStm...
 			Reference ref = validationStm.getReference();
+			// <reference>
 			sElement = eventFactory.createStartElement(prefix, uri, "reference");
 
 			eventWriter.add(sElement);
 			if (ref.getInternal() != null)
 				eventWriter.add(eventFactory.createAttribute("internal", ref.getInternal()));
 
+			// @quality
 			if (ref.getQuality() != null) // XXX aodl schema does actually not require quality attribute, but rodl editor does...
 				eventWriter.add(eventFactory.createAttribute("quality", ref.getQuality()));
 
-			// XXX authority attribute is not even a part in aodl schema reference definition...!
-			// XXX but it is in podl schema!!!
-			if (ref.getAuthority() != null) {
-				eventWriter.add(eventFactory.createAttribute("authority", ref.getAuthority().toString()));
-			} else
-				eventWriter.add(eventFactory.createAttribute("authority", auth));
+			// authority attribute is not even a part in aodl schema reference definition...!
+			// but it is in podl schema!!!
+			// @authority - only if serializing Person object
+			if (pdrObject instanceof Person)
+				if (ref.getAuthority() != null) {
+					eventWriter.add(eventFactory.createAttribute("authority", ref.getAuthority().toString()));
+				} else
+					eventWriter.add(eventFactory.createAttribute("authority", auth));
 
 			// Create Content
 			if (ref.getSourceId() != null) {

org.bbaw.pdr.ae.control.datahandling.xqj/src/org/bbaw/pdr/ae/control/datahandling/xqj/internal/RepositoryUpdateManager.java
@@ -318,6 +318,9 @@ public class RepositoryUpdateManager implements IUpdateManager
 
 	/**
 	 * Gets the modified aspects.
+	 * <p>Obtains a list of IDs of modified aspects from {@link PdrIdService},
+	 * looks up the corresponding XML serializations using {@link MainSearcher},
+	 * removes aodl ns prefixes and returns the results.</p>
 	 * @return the modified aspects
 	 * @throws XQException the xQ exception
 	 * @throws XMLStreamException the xML stream exception
@@ -1637,7 +1640,7 @@ public class RepositoryUpdateManager implements IUpdateManager
 				}
 				// check for loops
 				if (dependents.contains(i)) {
-					System.out.println("Detected object dependency loop at object "+i);
+					log(2, "Detected object dependency loop at object "+i);
 					// return all new local objects, all these objects failed to be ingested
 					return newObjsIds;
 				}
@@ -2355,6 +2358,8 @@ public class RepositoryUpdateManager implements IUpdateManager
 		// TODO wir muessen uns was ueberlegen falls keine user credentials zu haben sind. 
 		// manchmal geht user wechseln ja schief und dann muessen wir nochmal login dialog oeffnen oder so 
 		
+		// TODO bei modified objects sicherstellen dasz zwei revisions drinstehen, die letzte mit aktuellem zeitstempel
+		
 		String url = Platform.getPreferencesService().getString(CommonActivator.PLUGIN_ID, "REPOSITORY_URL",
 				AEConstants.REPOSITORY_URL, null);
 		_repositoryId = Platform.getPreferencesService().getInt(CommonActivator.PLUGIN_ID, "REPOSITORY_ID",
@@ -2488,7 +2493,7 @@ public class RepositoryUpdateManager implements IUpdateManager
 		}
 
 		//////////////////////////
-		// injest midified objects
+		// ingest modified objects
 		//////////////////////////
 		try	{
 			/////////////////